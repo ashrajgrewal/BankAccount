ee5132007eeb265d709a711097d97fe2077929e8
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-11-09 22:16:49 -0800

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,2 +1,7 @@
 #Bank Account Ashraj Grewal CS 1.1
-class BankAccount
+class BankAccount():
+    def __init__(self, full_name, account_number, routing_number, balance)
+    self.full_name = full_name
+    self.account_number = account_number
+    self.routing_number = routing_number
+    self.balance = balance
