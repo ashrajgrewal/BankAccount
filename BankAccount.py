1226e10e4c41f59efa1d5e70c8eb5036757c10f9
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-11-09 22:42:44 -0800

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,17 @@
 #Bank Account Ashraj Grewal CS 1.1
 class BankAccount():
-    def __init__(self, full_name, account_number, routing_number, balance):
+    def __init__(self, full_name, account_number, routing_number, balance=0):
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+        print("Amount Deposited:{}")
+    def withdraw(self, amount):
+
+    def get_balance():
+
+    def add_interest():
+
+    def print_receipt():
